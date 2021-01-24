9219e0e821d4df021f07441c96e58ad105a8746b
Serdar Yılmaz <yilmazz.serdar3955@gmail.com>
2021-01-24 17:47:03 +0300

    23Ocak Ders Sonu

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //Do ot repeat yourself = kendini tekrarlama
+            string kategoriEtiketi = "Kategoriler";
+            int ogreciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmismi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+
+
+
+
+
+            if(sistemeGirisYapmismi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarlar butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+
+
+            Console.WriteLine(kategoriEtiketi);
+            
+
+        }
+    }
+}
